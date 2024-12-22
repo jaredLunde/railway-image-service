e1301e0324ae917fab34b5ffe61955b2f2a75481
Jared Lunde <jared@railway.com>
2024-12-22 14:00:31 -0600

    mr clean

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"context"
-	"crypto/sha256"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/cshum/imagor"
-	"github.com/cshum/imagor/imagorpath"
-	"github.com/cshum/imagor/loader/httploader"
-	"github.com/cshum/imagor/storage/filestorage"
-	"github.com/cshum/imagor/vips"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/adaptor"
 	"github.com/gofiber/fiber/v3/middleware/favicon"
@@ -23,6 +16,7 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/helmet"
 	fiberrecover "github.com/gofiber/fiber/v3/middleware/recover"
 	"github.com/gofiber/fiber/v3/middleware/requestid"
+	"github.com/jaredLunde/railway-images/internal/app/imagor"
 	"github.com/jaredLunde/railway-images/internal/pkg/logger"
 	"github.com/jaredLunde/railway-images/internal/pkg/mw"
 )
@@ -42,62 +36,10 @@ func main() {
 		Pretty:   cfg.Environment == EnvironmentDevelopment,
 	})
 
-	imagorService := imagor.New(
-		imagor.WithLoaders(
-			// s3storage.New(),
-			// gcloudstorage.New(),
-			filestorage.New(
-				"./data",
-				filestorage.WithPathPrefix("/file"),
-				filestorage.WithSafeChars(""),
-			),
-			httploader.New(
-				httploader.WithForwardClientHeaders(false),
-				httploader.WithAccept("*/*"),
-				httploader.WithForwardHeaders(""),
-				httploader.WithOverrideResponseHeaders(""),
-				httploader.WithAllowedSources("*"),
-				httploader.WithAllowedSourceRegexps(""),
-				httploader.WithMaxAllowedSize(20*1024*1024),
-				httploader.WithInsecureSkipVerifyTransport(false),
-				httploader.WithDefaultScheme("https"),
-				httploader.WithBaseURL(""),
-				httploader.WithProxyTransport("", ""),
-				httploader.WithBlockLoopbackNetworks(false),
-				httploader.WithBlockPrivateNetworks(false),
-				httploader.WithBlockLinkLocalNetworks(false),
-				httploader.WithBlockNetworks(),
-			),
-		),
-		imagor.WithProcessors(vips.NewProcessor()),
-		imagor.WithSigner(imagorpath.NewHMACSigner(sha256.New, 0, "")),
-		imagor.WithBasePathRedirect(""),
-		imagor.WithBaseParams(""),
-		imagor.WithRequestTimeout(time.Second*30),
-		imagor.WithLoadTimeout(time.Second*30),
-		imagor.WithSaveTimeout(time.Second*30),
-		imagor.WithProcessTimeout(time.Second*30),
-		imagor.WithProcessConcurrency(20),
-		imagor.WithProcessQueueSize(100),
-		imagor.WithCacheHeaderTTL(time.Hour*24*7),
-		imagor.WithCacheHeaderSWR(time.Hour*24),
-		imagor.WithCacheHeaderNoCache(false),
-		imagor.WithAutoWebP(true),
-		imagor.WithAutoAVIF(true),
-		imagor.WithModifiedTimeCheck(false),
-		imagor.WithDisableErrorBody(false),
-		imagor.WithDisableParamsEndpoint(false),
-		imagor.WithResultStorages(filestorage.New("./tmp", filestorage.WithExpiration(time.Hour*24))),
-		imagor.WithStoragePathStyle(imagorpath.DigestStorageHasher),
-		imagor.WithResultStoragePathStyle(imagorpath.DigestResultStorageHasher),
-		imagor.WithUnsafe(cfg.Environment == EnvironmentDevelopment),
-		imagor.WithDebug(cfg.Environment == EnvironmentDevelopment),
-	)
-
-	appCtx, cancel := context.WithTimeout(ctx, time.Second*10)
-	defer cancel()
-
-	if err := imagorService.Startup(appCtx); err != nil {
+	imagorService, err := imagor.NewService(ctx, &imagor.Config{
+		Debug: cfg.Environment == EnvironmentDevelopment,
+	})
+	if err != nil {
 		log.Error("imagor app failed to start", "error", err)
 		os.Exit(1)
 	}
---
+++ internal/app/imagor/imagor.go
@@ -0,0 +1,85 @@
+package imagor
+
+import (
+	"context"
+	"crypto/sha256"
+	"time"
+
+	i "github.com/cshum/imagor"
+	"github.com/cshum/imagor/imagorpath"
+	"github.com/cshum/imagor/loader/httploader"
+	"github.com/cshum/imagor/storage/filestorage"
+	"github.com/cshum/imagor/vips"
+)
+
+type Config struct {
+	Debug bool
+}
+
+func NewService(ctx context.Context, cfg *Config) (*i.Imagor, error) {
+	debug := false
+	if cfg != nil {
+		debug = cfg.Debug
+	}
+
+	imagorService := i.New(
+		i.WithLoaders(
+			// s3storage.New(),
+			// gcloudstorage.New(),
+			filestorage.New(
+				"./data",
+				filestorage.WithPathPrefix("/file"),
+				filestorage.WithSafeChars(""),
+			),
+			httploader.New(
+				httploader.WithForwardClientHeaders(false),
+				httploader.WithAccept("*/*"),
+				httploader.WithForwardHeaders(""),
+				httploader.WithOverrideResponseHeaders(""),
+				httploader.WithAllowedSources("*"),
+				httploader.WithAllowedSourceRegexps(""),
+				httploader.WithMaxAllowedSize(20*1024*1024),
+				httploader.WithInsecureSkipVerifyTransport(false),
+				httploader.WithDefaultScheme("https"),
+				httploader.WithBaseURL(""),
+				httploader.WithProxyTransport("", ""),
+				httploader.WithBlockLoopbackNetworks(false),
+				httploader.WithBlockPrivateNetworks(false),
+				httploader.WithBlockLinkLocalNetworks(false),
+				httploader.WithBlockNetworks(),
+			),
+		),
+		i.WithProcessors(vips.NewProcessor()),
+		i.WithSigner(imagorpath.NewHMACSigner(sha256.New, 0, "")),
+		i.WithBasePathRedirect(""),
+		i.WithBaseParams(""),
+		i.WithRequestTimeout(time.Second*30),
+		i.WithLoadTimeout(time.Second*30),
+		i.WithSaveTimeout(time.Second*30),
+		i.WithProcessTimeout(time.Second*30),
+		i.WithProcessConcurrency(20),
+		i.WithProcessQueueSize(100),
+		i.WithCacheHeaderTTL(time.Hour*24*7),
+		i.WithCacheHeaderSWR(time.Hour*24),
+		i.WithCacheHeaderNoCache(false),
+		i.WithAutoWebP(true),
+		i.WithAutoAVIF(true),
+		i.WithModifiedTimeCheck(false),
+		i.WithDisableErrorBody(false),
+		i.WithDisableParamsEndpoint(false),
+		i.WithResultStorages(filestorage.New("./tmp", filestorage.WithExpiration(time.Hour*24))),
+		i.WithStoragePathStyle(imagorpath.DigestStorageHasher),
+		i.WithResultStoragePathStyle(imagorpath.DigestResultStorageHasher),
+		i.WithUnsafe(debug),
+		i.WithDebug(debug),
+	)
+
+	appCtx, cancel := context.WithTimeout(ctx, time.Second*10)
+	defer cancel()
+
+	if err := imagorService.Startup(appCtx); err != nil {
+		return nil, err
+	}
+
+	return imagorService, nil
+}
