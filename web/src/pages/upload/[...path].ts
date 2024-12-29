7243f124a578b793cbbae73d84ea9d553691d1ce
Jared Lunde <jared.lunde@gmail.com>
2024-12-29 10:30:56 -0800

    add docker image release (#2)

--- web/src/pages/upload/[...path].ts
+++ web/src/pages/upload/[...path].ts
@@ -5,22 +5,22 @@ import { ImageServiceClient } from "railway-image-service/server";
 export const prerender = false;
 
 const client = new ImageServiceClient({
-  url: import.meta.env.IMAGE_SERVICE_URL,
-  secretKey: import.meta.env.IMAGE_SERVICE_SECRET_KEY,
-  signatureSecretKey: import.meta.env.IMAGE_SERVICE_SIGNATURE_SECRET_KEY,
+	url: import.meta.env.IMAGE_SERVICE_URL,
+	secretKey: import.meta.env.IMAGE_SERVICE_SECRET_KEY,
+	signatureSecretKey: import.meta.env.IMAGE_SERVICE_SIGNATURE_SECRET_KEY,
 });
 
 export const PUT: APIRoute = async ({ params }) => {
-  if (!params.path) {
-    return new Response("Missing path", { status: 400 });
-  }
+	if (!params.path) {
+		return new Response("Missing path", { status: 400 });
+	}
 
-  return new Response(null, {
-    status: 301,
-    headers: {
-      Method: "PUT",
-      Location: await client.sign(`blob/${params.path}`),
-      "Cache-Control": "private, max-age=0, s-maxage=0", // Never cache these because they have expiring URLs
-    },
-  });
+	return new Response(null, {
+		status: 301,
+		headers: {
+			Method: "PUT",
+			Location: await client.sign(`blob/${params.path}`),
+			"Cache-Control": "private, max-age=0, s-maxage=0", // Never cache these because they have expiring URLs
+		},
+	});
 };
